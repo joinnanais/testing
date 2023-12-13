07564a68257794e53c782b95e4f4b3e63e72dc3b
Joinna Anais <81961351+joinnanais@users.noreply.github.com>
2023-12-13 14:06:55 +0000

    updated hello page

--- Hello.py
+++ Hello.py
@@ -15,36 +15,35 @@
 import streamlit as st
 from streamlit.logger import get_logger
 import pandas as pd
+import plotly as px
 
 LOGGER = get_logger(__name__)
 
 
 def run():
     st.set_page_config(
-        page_title="Welcome",
-        page_icon=":bar chart:",
+        page_title="Sales Dashboard",
+        page_icon=":bar_chart:",
     )
 
-    st.write("# Sales Dashboard")
+    st.write("# Sales Dashboard :bar_chart:")
+
+    st.sidebar.success("Select a demo above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+        This is a prototype dashboard to demonstrate the concept of Streamlit for CDP purposes.
+        
 
+        The data is from an open-source.
+        
+        
+        The data used is a sample data from Kaggle.
+        """
+    )
 
 if __name__ == "__main__":
     run()
+
+
+
---
+++ pages/Dashboard Demo.py
@@ -0,0 +1,43 @@
+# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from urllib.error import URLError
+
+import altair as alt
+import pandas as pd
+
+import streamlit as st
+from streamlit.hello.utils import show_code
+
+
+github_csv_url = 'https://raw.githubusercontent.com/joinnanais/Elemis/main/Sample%20-%20Superstore.csv'
+df = pd.read_csv(github_csv_url, encoding='latin1')
+
+st.sidebar.header("Filters")
+
+# Select category filter
+category_filter = st.sidebar.multiselect("Select Category", df['Category'].unique(), df['Category'].unique())
+
+# Select sub-category filter
+sub_category_filter = st.sidebar.multiselect("Select Sub-Category", df['Sub-Category'].unique(), df['Sub-Category'].unique())
+
+filtered_df = df[(df['Category'].isin(category_filter)) & 
+                 (df['Sub-Category'].isin(sub_category_filter))]
+
+# Display the filtered dataframe
+st.subheader("Filtered Data")
+st.write(filtered_df)
+
+
+
