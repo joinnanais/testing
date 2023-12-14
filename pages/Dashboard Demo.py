36a581c90cd961cc2bc18332235f284fa04e04af
Joinna Anais <81961351+joinnanais@users.noreply.github.com>
2023-12-14 11:36:07 +0000

    New filters

--- pages/Dashboard Demo.py
+++ pages/Dashboard Demo.py
@@ -14,29 +14,37 @@
 
 import altair as alt
 import pandas as pd
-
 import streamlit as st
 from streamlit.hello.utils import show_code
 from urllib.error import URLError
 
 st.set_page_config(
         page_title="Sales Dashboard",
-        page_icon=":bar_chart:")
+        page_icon=":bar_chart:",
+        layout = "wide",
+        menu_items={
+        'Get Help': "https://uk.elemis.com/featured/special-promotion?gad_source=1&gclid=Cj0KCQiA7OqrBhD9ARIsAK3UXh2MCbFg-Fckqjejakv8MtT7BI4D9ggNA1dKUYcC4BB4AE3CVWaqpVsaAuAqEALw_wcB",
+        'Report a bug': "https://uk.elemis.com/featured/special-promotion?gad_source=1&gclid=Cj0KCQiA7OqrBhD9ARIsAK3UXh2MCbFg-Fckqjejakv8MtT7BI4D9ggNA1dKUYcC4BB4AE3CVWaqpVsaAuAqEALw_wcB",
+        'About': "# This app is designed for CDP prototype - this is a test dashboard"}
+        )
 
 st.markdown(
         """
+        # Sales Dashboard :bar_chart:
+
         This is a prototype dashboard to demonstrate the concept of Streamlit for CDP purposes.
         
 
         The data is from an open-source.
         
         
-        The data used is a sample data from Kaggle.
+        The data used is a superstore sample data from Kaggle which contains sales and profit.
         """
     )
 
 github_csv_url = 'https://raw.githubusercontent.com/joinnanais/Elemis/main/Sample%20-%20Superstore.csv'
-df = pd.read_csv(github_csv_url, encoding='latin1')
+data = pd.read_csv(github_csv_url, encoding='latin1')
+df = pd.DataFrame(data)
 
 st.sidebar.header("Filters")
 
@@ -46,14 +54,47 @@ category_filter = st.sidebar.multiselect("Select Category", df['Category'].uniqu
 # Select sub-category filter
 sub_category_filter = st.sidebar.multiselect("Select Sub-Category", df['Sub-Category'].unique(), df['Sub-Category'].unique())
 
+# Select city filter
+city_filter = st.sidebar.multiselect("Select City", df['City'].unique(), df['City'].unique())
+
+# Select Region
+region_filter = st.sidebar.multiselect("Select Region", df['Region'].unique(), df['Region'].unique())
+
+# Select Product Name
+product_filter = st.sidebar.multiselect("Select Product Name", df['Product Name'].unique(), df['Product Name'].unique())
+
+# Select Segment 
+segment_filter = st.sidebar.multiselect("Select Segment", df['Segment'].unique(), df['Segment'].unique())
+
+# Date Filter
+# Date range slider
+# Convert dates 
+
+# Convert 'Order Date' to datetime
+df["Order Date"] = pd.to_datetime(df["Order Date"])
+
+# Streamlit app
+st.title('Date Filter Example')
+
+# Set default values within the range of the data
+default_start_date = df['Order Date'].min().date()
+default_end_date = df['Order Date'].max().date()
+
+# Date range slider with default values
+start_date = st.date_input('Start Date', default_start_date, min_value=df['Order Date'].min().date(), max_value=df['Order Date'].max().date())
+end_date = st.date_input('End Date', default_end_date, min_value=df['Order Date'].min().date(), max_value=df['Order Date'].max().date())
+
 filtered_df = df[(df['Category'].isin(category_filter)) & 
-                 (df['Sub-Category'].isin(sub_category_filter))]
+                 (df['Sub-Category'].isin(sub_category_filter)) &
+                 (df['City'].isin(city_filter)) &
+                 (df['Region'].isin(region_filter)) &
+                 (df['Product Name'].isin(product_filter)) &
+                 (df['Segment'].isin(segment_filter)) &
+                 (df['Order Date'] >= pd.to_datetime(start_date)) & 
+                 (df['Order Date'] <= pd.to_datetime(end_date))
+                 ] 
 
 # Display the filtered dataframe
 st.subheader("Filtered Data")
 st.write(filtered_df)
-
-
-
-
 
